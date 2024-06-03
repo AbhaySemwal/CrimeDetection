62f6c98e9e08f2da8e0f06a8cf2ddd0b1c312870
AbhaySemwal <shbhtsemwal@gmail.com>
2024-06-03 15:22:09 +0530

    cleaning up code

--- Website/backend/app.py
+++ Website/backend/app.py
@@ -9,12 +9,10 @@ app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Load your trained .h5 model
-model = tf.keras.models.load_model('/home/shobhits/Documents/Mini Project/CrimeDetection/Website/backend/Models/1717240268.h5')
+model = tf.keras.models.load_model('Models/1717240268.h5')
 
 def preprocess_frame(frame):
-    # Resize and normalize the frame as required by your model
-    frame = cv2.resize(frame, (150, 150))  # Example size, adjust to your model's input size
+    frame = cv2.resize(frame, (150, 150))
     frame = frame / 255.0
     return np.expand_dims(frame, axis=0)
 
@@ -29,17 +27,13 @@ def generate_frames():
         preprocessed_frame = preprocess_frame(frame)
         prediction = model.predict(preprocessed_frame)
         label = 'Suspicious' if prediction[0] > 0.85 else 'Normal'
-
-        # Print prediction value to the console
+        
         print(f"Prediction: {prediction[0]}, Label: {label}")
 
-        # Send prediction to the frontend via WebSocket
         socketio.emit('prediction', {'prediction': float(prediction[0]), 'label': label}, namespace='/')
 
-        # Overlay the prediction on the frame
         cv2.putText(frame, label, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        # Convert frame to JPEG
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
